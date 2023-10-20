aa55c9ce9eeaaf6f2adeef8234a1aef3b44ac9a1
Santiago Tapasco Giraldo <santy1446@gmail.com>
2023-10-20 18:40:47 -0500

    style: 💄 Welcome page

--- projects/mf-container/src/app/ui/pages/home/home.component.ts
+++ projects/mf-container/src/app/ui/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CognitoSessionResponse, CognitoUserCommonService, UserSessionDataResponse } from 'commons-lib';
+import { CognitoUserCommonService, UserSessionDataResponse } from 'commons-lib';
 
 @Component({
   templateUrl: './home.component.html',
@@ -7,17 +7,17 @@ import { CognitoSessionResponse, CognitoUserCommonService, UserSessionDataRespon
 })
 export class HomeComponent {
 
-  constructor(private _cognitoUserCommonService: CognitoUserCommonService){}
+  userName: string = "";
+
+  constructor(private _cognitoUserCommonService: CognitoUserCommonService){
+    this.getUserInfo();
+  }
 
   getUserInfo() {
     this._cognitoUserCommonService.getUser().subscribe({
-      next: (res: UserSessionDataResponse) => console.log("User info: ", res)
-    })
-  }
-  getSessionInfo() {
-    this._cognitoUserCommonService.getSession().subscribe({
-      next: (res: CognitoSessionResponse) => console.log("Session info: ", res)
-      
+      next: (res: UserSessionDataResponse) => {
+        this.userName = res.attributes.nickname;
+      }
     })
   }
 }
